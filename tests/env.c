cc9273e35ee3ea9a02f06e721479b0aa4477a248
janson <gandancing@gmail.com>
2015-07-27 18:39:31 +0800

    todo for env, but it seen not work

--- tests/env.c
+++ tests/env.c
@@ -4,8 +4,24 @@
 #include <unistd.h>
 
 //TODO for shell env
-int main(int argc, char **argv)
+int main(int argc, char **argv, char * envp[])
 {
+    char** env;
+    char* this_env;
+    char *v;
+
+    for (env = envp; *env != 0; env++) {
+        this_env = (char*)strdup(*env);
+        v = strstr(this_env, "=");
+        if(v != NULL) {
+            *v = '\0';
+            v += 1;
+            printf("%s=%s\n", this_env, v);
+        }
+        if(this_env != NULL) {
+            free(this_env);
+        }
+    }
   putenv("SomeVariable=SomeValue");
   return 0;
 }
